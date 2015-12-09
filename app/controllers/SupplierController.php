0f239cb804cd205205ab790de98b3c49e67dd218
xuzhaoyu <xuzhaoyu@gmail.com>
2015-12-09 11:37:34 +0800

    supplier

--- app/controllers/SupplierController.php
+++ app/controllers/SupplierController.php
@@ -7,12 +7,14 @@
  */
 class SupplierController extends \BaseController{
     public function getIncomplete(){
-        $inc = Orders::where('status','<>', 'complete')->get();
-        return View::make('Supplier.incomplete')->with('orders',$inc);
+        $inc= Orders::where('status','<>', 'complete')->orderBy('orderNum','DESC')->get();
+        $num= Orders::where('status','<>', 'complete')->groupBy('orderNum')->orderBy('orderNum','DESC')->get();
+        return View::make('Supplier.incomplete')->with('orders',$inc)->with('nums', $num);
     }
 
     public function getComplete(){
-        $comp= Orders::where('status','=', 'complete')->get();
-        return View::make('Supplier.complete')->with('orders',$comp);
+        $comp= Orders::where('status','=', 'complete')->orderBy('orderNum','DESC')->get();
+        $num= Orders::where('status','=', 'complete')->groupBy('orderNum')->orderBy('orderNum','DESC')->get();
+        return View::make('Supplier.complete')->with('orders',$comp)->with('nums', $num);
     }
 }
