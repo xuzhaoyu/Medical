4ee13ca77286b8efe257a5c938dd2c8f1f7f8995
xuzhaoyu <xuzhaoyu@gmail.com>
2015-12-12 12:13:33 +0800

    send email

--- app/controllers/SupplierController.php
+++ app/controllers/SupplierController.php
@@ -10,15 +10,15 @@ class SupplierController extends \BaseController
 {
     public function getIncomplete()
     {
-        $inc = Orders::where('status', '<>', 'complete')->orderBy('orderNum', 'DESC')->get();
-        $num = Orders::where('status', '<>', 'complete')->groupBy('orderNum')->orderBy('orderNum', 'DESC')->get();
+        $inc = Orders::where('status', '<>', 'complete')->where('SId', '=', Auth::user()->id)->orderBy('orderNum', 'DESC')->get();
+        $num = Orders::where('status', '<>', 'complete')->where('SId', '=', Auth::user()->id)->groupBy('orderNum')->orderBy('orderNum', 'DESC')->get();
         return View::make('Supplier.incomplete')->with('orders', $inc)->with('nums', $num);
     }
 
     public function getComplete()
     {
-        $comp = Orders::where('status', '=', 'complete')->orderBy('orderNum', 'DESC')->get();
-        $num = Orders::where('status', '=', 'complete')->groupBy('orderNum')->orderBy('orderNum', 'DESC')->get();
+        $comp = Orders::where('status', '=', 'complete')->where('SId', '=', Auth::user()->id)->orderBy('orderNum', 'DESC')->get();
+        $num = Orders::where('status', '=', 'complete')->where('SId', '=', Auth::user()->id)->groupBy('orderNum')->orderBy('orderNum', 'DESC')->get();
         //$page = Paginator::make($num, count($num), 1);
         return View::make('Supplier.complete')->with('orders', $comp)->with('nums', $num);
     }
